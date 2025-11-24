d11e125532e9f2fe7727cd80f464379e73bacc5d
your name <photo@cardinalphoto.com>
2025-11-24 08:29:44 -0800

    fix: parse geocode results, fix location-log CommonJS, update locate-me styling to match partner page

--- api/location-log.js
+++ api/location-log.js
@@ -1,6 +1,6 @@
 const { readLog } = require('../lib/locationLog');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
